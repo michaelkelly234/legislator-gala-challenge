e477d6d60a89068155971f7a7bd1195bb4a0110a
Michael Kelly <michaelkelly234@gmail.com>
2023-02-17 06:06:59 -0500

    solve list and expand test cases

--- source_test.py
+++ source_test.py
@@ -1,29 +1,13 @@
 import source
 
 
-def test1(noOfGuests, num_tables):
+def test(noOfGuests, num_tables, planner_preferences):
     #Test and presets
-    noOfGuests=10
     guest_list=list(range(1, noOfGuests+1))
 
 
     #User input
-    num_tables = 2
     guest_list=["name_"+str(x) for x in guest_list]
-    planner_preferences = [
-        {
-            "preference": "avoid",
-            "guests": ["name_1", "name_2"]
-        },
-        {
-            "preference": "avoid",
-            "guests": ["name_2", "name_4"]
-        },
-        {
-            "preference": "avoid",
-            "guests": ["name_1", "name_4"]
-        }
-    ]
 
     seating_list = source.seating_list(num_tables, guest_list, planner_preferences)
     seating_list.solve()
@@ -32,4 +16,111 @@ def test1(noOfGuests, num_tables):
     
 
 if __name__ == "__main__":
-    main()
+
+    try:
+        planner_preferences = [
+            {
+                "preference": "avoid",
+                "guests": ["name_1", "name_2"]
+            },
+            {
+                "preference": "avoid",
+                "guests": ["name_2", "name_3"]
+            },
+            {
+                "preference": "avoid",
+                "guests": ["name_1", "name_3"]
+            }
+        ]
+        test(10, 3, planner_preferences)
+    except Exception as e: print(e)
+
+    try:
+        planner_preferences = [
+            {
+                "preference": "avoid",
+                "guests": ["name_1", "name_2"]
+            },
+            {
+                "preference": "avoid",
+                "guests": ["name_2", "name_3"]
+            },
+            {
+                "preference": "avoid",
+                "guests": ["name_1", "name_3"]
+            }
+        ]
+        test(10, 1, planner_preferences)
+    except Exception as e: print(e)
+
+    try:
+        planner_preferences = [
+            {
+                "preference": "avoid",
+                "guests": ["name_1", "name_2"]
+            },
+            {
+                "preference": "avoid",
+                "guests": ["name_2", "name_3"]
+            },
+            {
+                "preference": "avoid",
+                "guests": ["name_1", "name_3"]
+            }
+        ]
+        test(10, 2, planner_preferences)
+    except Exception as e: print(e)
+
+    try:
+        planner_preferences = [
+            {
+                "preference": "pair",
+                "guests": ["name_1", "name_2"]
+            },
+            {
+                "preference": "pair",
+                "guests": ["name_2", "name_3"]
+            },
+            {
+                "preference": "avoid",
+                "guests": ["name_1", "name_3"]
+            }
+        ]
+        test(10, 2, planner_preferences)
+    except Exception as e: print(e)
+
+    try:
+        planner_preferences = [
+            {
+                "preference": "pair",
+                "guests": ["name_1", "name_2"]
+            },
+            {
+                "preference": "pair",
+                "guests": ["name_2", "name_3"]
+            },
+            {
+                "preference": "avoid",
+                "guests": ["name_4", "name_5"]
+            }
+        ]
+        test(10, 15, planner_preferences)
+    except Exception as e: print(e)
+
+    try:
+        planner_preferences = [
+            {
+                "preference": "pair",
+                "guests": ["name_1", "name_2"]
+            },
+            {
+                "preference": "pair",
+                "guests": ["name_2", "name_3"]
+            },
+            {
+                "preference": "avoid",
+                "guests": ["name_4", "name_5"]
+            }
+        ]
+        test(2, 15, planner_preferences)
+    except Exception as e: print(e)
